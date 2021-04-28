cd1d1c512feedb14c32ab3db640087997df310d9
longnghts <82634370+longnghts@users.noreply.github.com>
2021-04-28 01:27:00 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function inc1(){
+    var likeCount = document.querySelector("#likesNeil");
+    likeCount.innerHTML = parseInt(likeCount.textContent) + 1;
+}
+function inc2(){
+    var likeCount = document.querySelector("#likesNichole");
+    likeCount.innerHTML = parseInt(likeCount.textContent) + 1;
+}
+function inc3(){
+    var likeCount = document.querySelector("#likesJim");
+    likeCount.innerHTML = parseInt(likeCount.textContent) + 1;
+}
